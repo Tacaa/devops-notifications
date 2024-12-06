e9deb3f9c6b253280efe967191ee55be8e254d01
Tatjana Gavrilović <tatjana.gavrilovic@openfyle.com>
2024-12-06 20:11:07 +0100

    init: Notifications service set up

---
+++ src/main/java/com/devops/devops_notifications/DevopsNotificationsApplication.java
@@ -0,0 +1,13 @@
+package com.devops.devops_notifications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsNotificationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsNotificationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devops/devops_notifications/DevopsNotificationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.devops_notifications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsNotificationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
