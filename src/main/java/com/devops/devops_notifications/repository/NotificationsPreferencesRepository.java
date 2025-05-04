a611cc52a0f11fc40d7d1d7145c474ec661c779f
Tatjana Gavrilović <tatjana.gavrilovic@openfyle.com>
2025-05-04 16:32:05 +0200

    feat: add observability configuration, add logs

--- src/main/java/com/devops/devops_notifications/repository/NotificationsPreferencesRepository.java
+++ src/main/java/com/devops/devops_notifications/repository/NotificationsPreferencesRepository.java
@@ -1,8 +1,10 @@
 package com.devops.devops_notifications.repository;
 
 import com.devops.devops_notifications.model.NotificationPreference;
+import io.micrometer.observation.annotation.Observed;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@Observed
 public interface NotificationsPreferencesRepository extends JpaRepository<NotificationPreference, Integer> {
 
     NotificationPreference findByUserId(Integer userId);
